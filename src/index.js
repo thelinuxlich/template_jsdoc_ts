107ffaac261b5cb7950a3e8a7191dc75802bee45
Alisson <thelinuxlich@gmail.com>
2022-03-24 13:10:29 -0300

    fix(core): make the template run green on test

--- src/index.js
+++ src/index.js
@@ -1,38 +1,22 @@
 // @ts-check
 /** @typedef { import("./types.js").Animal } Animal */
 
-/** @type {string} */
-let foo = 'a'
-
 /**
- *
- *
  * @param {Animal} foo
  * @param {string} bar
  * @returns {string}
  */
 let bar = (foo, bar) => 'test'
 
-/**
- *
- *
- * @param {string} test
- * @returns {number}
- */
-let biz = test => 123
-
-/** @type {string} */
-const b = 'a'
-
 /** @type {Animal} */
 let dog = {
-  alive: 'b',
+  alive: true,
   hp: 1,
 }
 
 // in-source test suites
 if (import.meta.vitest) {
   it('dog is alive', () => {
-    expect(dog.alive).toBe(false)
+    expect(dog.alive).toBe(true)
   })
 }
